7fe00ca336d253a8fdac206fd4e6734e13554a61
Irimia-Sorici Andrei <andreisorici2007@gmail.com>
2024-01-09 10:08:06 +0200

    reparat textura la drum

--- EAR3-NewYearNewSkills2024-game/Assets/Scripts/Resource.cs
+++ EAR3-NewYearNewSkills2024-game/Assets/Scripts/Resource.cs
@@ -5,12 +5,4 @@ using UnityEngine;
 public class Resource : MonoBehaviour
 {
     public string resource;
-
-    void OnColliderStay2D(Collider2D other)
-    {
-        if(other.tag=="GetResources")
-        {
-            other.gameObject.transform.parent.GetComponent<CityStorage>().AddResource(resource);
-        }
-    }
 }
