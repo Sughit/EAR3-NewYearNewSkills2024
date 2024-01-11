2b79089fd569d86573cb611693030189bb0db917
Irimia-Sorici Andrei <andreisorici2007@gmail.com>
2024-01-11 21:18:39 +0200

    reparat colidere

--- EAR3-NewYearNewSkills2024-game/Assets/Scripts/CityStorage.cs
+++ EAR3-NewYearNewSkills2024-game/Assets/Scripts/CityStorage.cs
@@ -8,26 +8,26 @@ public class CityStorage : MonoBehaviour
     public List<string> resourcesList = new List<string>();
     public float resourceRange = 2;
     public LayerMask resourcesMask;
-    public int wood;
-    public int oil;
-    public int rock;
-    public int sand;
+    // public int wood;
+    // public int oil;
+    // public int rock;
+    // public int sand;
 
-    bool canAddResources=true;
+    // bool canAddResources=true;
 
-    [HideInInspector]
-    public bool canAddWood;
-    [HideInInspector]
-    public bool canAddOil;
-    [HideInInspector]
-    public bool canAddRock;
-    [HideInInspector]
-    public bool canAddSand;
+    // [HideInInspector]
+    // public bool canAddWood;
+    // [HideInInspector]
+    // public bool canAddOil;
+    // [HideInInspector]
+    // public bool canAddRock;
+    // [HideInInspector]
+    // public bool canAddSand;
 
-    public float timeToAddWood=2f;
-    public float timeToAddOil=2f;
-    public float timeToAddRock=2f;
-    public float timeToAddSand=2f;
+    // public float timeToAddWood=2f;
+    // public float timeToAddOil=2f;
+    // public float timeToAddRock=2f;
+    // public float timeToAddSand=2f;
 
     public Transform iconsParent;
     public GameObject resourceIcon;
@@ -51,25 +51,25 @@ public class CityStorage : MonoBehaviour
             switch(other.gameObject.tag)
             {
                 case "Wood":
-                canAddWood=true;
+                //canAddWood=true;
                 GameObject resource = Instantiate(resourceIcon, iconsParent);
                 resource.GetComponentInChildren<Image>().sprite = sprites[0];
                 break;
 
                 case "Oil":
-                canAddOil=true;
+                //canAddOil=true;
                 resource = Instantiate(resourceIcon, iconsParent);
                 resource.GetComponentInChildren<Image>().sprite = sprites[1];
                 break;
 
                 case "Rock":
-                canAddRock=true;
+                //canAddRock=true;
                 resource = Instantiate(resourceIcon, iconsParent);
                 resource.GetComponentInChildren<Image>().sprite = sprites[2];
                 break;
 
                 case "Sand":
-                canAddSand=true;
+                //canAddSand=true;
                 resource = Instantiate(resourceIcon, iconsParent);
                 resource.GetComponentInChildren<Image>().sprite = sprites[3];
                 break;
@@ -81,104 +81,79 @@ public class CityStorage : MonoBehaviour
         }
     }
 
-    void Update()
-    {
-        if(resourcesList.Count > 0) 
-        {
-            if(canAddResources)
-            {
-                if(canAddWood)
-                {
-                    AddWoodToCity();
-                    canAddWood=false;
-                    Invoke("ResetTimerWood", timeToAddWood);
-                }
-                if(canAddOil)
-                {
-                    AddOilToCity();
-                    canAddOil=false;
-                    Invoke("ResetTimerOil", timeToAddOil);
-                }
-                if(canAddRock)
-                {
-                    AddRockToCity();
-                    canAddRock=false;
-                    Invoke("ResetTimerRock", timeToAddRock);
-                }
-                if(canAddSand)
-                {
-                    AddSandToCity();
-                    canAddSand=false;
-                    Invoke("ResetTimerSand", timeToAddSand);
-                }
-            }
-        }
-    }
+    // void Update()
+    // {
+    //     if(resourcesList.Count > 0) 
+    //     {
+    //         if(canAddResources)
+    //         {
+    //             if(canAddWood)
+    //             {
+    //                 AddWoodToCity();
+    //                 canAddWood=false;
+    //                 Invoke("ResetTimerWood", timeToAddWood);
+    //             }
+    //             if(canAddOil)
+    //             {
+    //                 AddOilToCity();
+    //                 canAddOil=false;
+    //                 Invoke("ResetTimerOil", timeToAddOil);
+    //             }
+    //             if(canAddRock)
+    //             {
+    //                 AddRockToCity();
+    //                 canAddRock=false;
+    //                 Invoke("ResetTimerRock", timeToAddRock);
+    //             }
+    //             if(canAddSand)
+    //             {
+    //                 AddSandToCity();
+    //                 canAddSand=false;
+    //                 Invoke("ResetTimerSand", timeToAddSand);
+    //             }
+    //         }
+    //     }
+    // }
 
-    //functii de adaugat resurse
-    void AddWoodToCity()
-    {
-        wood++;
-    }
-    void AddOilToCity()
-    {
-        oil++;
-    }
-    void AddRockToCity()
-    {
-        rock++;
-    }
-    void AddSandToCity()
-    {
-        sand++;
-    }
+    // //functii de adaugat resurse
+    // void AddWoodToCity()
+    // {
+    //     wood++;
+    // }
+    // void AddOilToCity()
+    // {
+    //     oil++;
+    // }
+    // void AddRockToCity()
+    // {
+    //     rock++;
+    // }
+    // void AddSandToCity()
+    // {
+    //     sand++;
+    // }
 
-    //functii de resetat timpul
-    void ResetTimerWood()
-    {
-        canAddWood=true;
-    }
-    void ResetTimerOil()
-    {
-        canAddOil=true;
-    }
-    void ResetTimerRock()
-    {
-        canAddRock=true;
-    }
-    void ResetTimerSand()
-    {
-        canAddSand=true;
-    }
+    // //functii de resetat timpul
+    // void ResetTimerWood()
+    // {
+    //     canAddWood=true;
+    // }
+    // void ResetTimerOil()
+    // {
+    //     canAddOil=true;
+    // }
+    // void ResetTimerRock()
+    // {
+    //     canAddRock=true;
+    // }
+    // void ResetTimerSand()
+    // {
+    //     canAddSand=true;
+    // }
 
     void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(this.gameObject.transform.position, resourceRange);
     }
-
-    //void AddResourcesToCity()
-    // {
-    //     foreach(var resource in resourcesList)
-    //     {
-    //         switch(resource)
-    //         {
-    //             case "Wood":
-    //             wood++;
-    //             break;
-    //             case "Oil":
-    //             oil++;
-    //             break;
-    //             case "Rock":
-    //             rock++;
-    //             break;
-    //             case "Sand":
-    //             sand++;
-    //             break;
-    //             default:
-    //             Debug.Log("This city doesn't have that resource or that resource doesn't exist");
-    //             break;
-    //         }
-    //     }
-    // }
 }
--- EAR3-NewYearNewSkills2024-game/Assets/Scripts/Resource.cs
+++ EAR3-NewYearNewSkills2024-game/Assets/Scripts/Resource.cs
@@ -35,92 +35,92 @@ public class Resource : MonoBehaviour
         if(cityA.GetComponent<CityStorage>().resourcesList.Contains("Wood") && !cityB.GetComponent<CityStorage>().resourcesList.Contains("Wood"))
         {
             cityB.GetComponent<CityStorage>().resourcesList.Add("Wood");
-            cityB.GetComponent<CityStorage>().timeToAddWood += 1;
-            cityB.GetComponent<CityStorage>().canAddWood=true;
+            // cityB.GetComponent<CityStorage>().timeToAddWood += 1;
+            // cityB.GetComponent<CityStorage>().canAddWood=true;
             GameObject resource = Instantiate(resourceIcon, cityB.GetComponent<CityStorage>().iconsParent);
             resource.GetComponentInChildren<Image>().sprite = cityB.GetComponent<CityStorage>().sprites[0];
 
-            cityA.GetComponent<CityStorage>().timeToAddWood += 1;
+            // cityA.GetComponent<CityStorage>().timeToAddWood += 1;
         }
         //daca cityB are lemn si cityA nu
         else if(!cityA.GetComponent<CityStorage>().resourcesList.Contains("Wood") && cityB.GetComponent<CityStorage>().resourcesList.Contains("Wood"))
         {
             cityA.GetComponent<CityStorage>().resourcesList.Add("Wood");
-            cityA.GetComponent<CityStorage>().timeToAddWood += 1;
-            cityA.GetComponent<CityStorage>().canAddWood=true;
+            // cityA.GetComponent<CityStorage>().timeToAddWood += 1;
+            // cityA.GetComponent<CityStorage>().canAddWood=true;
             GameObject resource = Instantiate(resourceIcon, cityA.GetComponent<CityStorage>().iconsParent);
             resource.GetComponentInChildren<Image>().sprite = cityA.GetComponent<CityStorage>().sprites[0];
 
-            cityB.GetComponent<CityStorage>().timeToAddWood += 1;
+            // cityB.GetComponent<CityStorage>().timeToAddWood += 1;
         }
 
         //daca cityA are ulei si cityB nu
         if(cityA.GetComponent<CityStorage>().resourcesList.Contains("Oil") && !cityB.GetComponent<CityStorage>().resourcesList.Contains("Oil"))
         {
             cityB.GetComponent<CityStorage>().resourcesList.Add("Oil");
-            cityB.GetComponent<CityStorage>().timeToAddOil += 1;
-            cityB.GetComponent<CityStorage>().canAddOil=true;
+            // cityB.GetComponent<CityStorage>().timeToAddOil += 1;
+            // cityB.GetComponent<CityStorage>().canAddOil=true;
             GameObject resource = Instantiate(resourceIcon, cityB.GetComponent<CityStorage>().iconsParent);
             resource.GetComponentInChildren<Image>().sprite = cityB.GetComponent<CityStorage>().sprites[1];
 
-            cityA.GetComponent<CityStorage>().timeToAddOil += 1;
+            // cityA.GetComponent<CityStorage>().timeToAddOil += 1;
         }
         //daca cityB are ulei si cityA nu
         else if(!cityA.GetComponent<CityStorage>().resourcesList.Contains("Oil") && cityB.GetComponent<CityStorage>().resourcesList.Contains("Oil"))
         {
             cityA.GetComponent<CityStorage>().resourcesList.Add("Oil");
-            cityA.GetComponent<CityStorage>().timeToAddOil += 1;
-            cityA.GetComponent<CityStorage>().canAddOil=true;
+            // cityA.GetComponent<CityStorage>().timeToAddOil += 1;
+            // cityA.GetComponent<CityStorage>().canAddOil=true;
             GameObject resource = Instantiate(resourceIcon, cityA.GetComponent<CityStorage>().iconsParent);
             resource.GetComponentInChildren<Image>().sprite = cityA.GetComponent<CityStorage>().sprites[1];
 
-            cityB.GetComponent<CityStorage>().timeToAddOil += 1;
+            // cityB.GetComponent<CityStorage>().timeToAddOil += 1;
         }
 
         //daca cityA are piatra si cityB nu
         if(cityA.GetComponent<CityStorage>().resourcesList.Contains("Rock") && !cityB.GetComponent<CityStorage>().resourcesList.Contains("Rock"))
         {
             cityB.GetComponent<CityStorage>().resourcesList.Add("Rock");
-            cityB.GetComponent<CityStorage>().timeToAddRock += 1;
-            cityB.GetComponent<CityStorage>().canAddRock=true;
+            // cityB.GetComponent<CityStorage>().timeToAddRock += 1;
+            // cityB.GetComponent<CityStorage>().canAddRock=true;
             GameObject resource = Instantiate(resourceIcon, cityB.GetComponent<CityStorage>().iconsParent);
             resource.GetComponentInChildren<Image>().sprite = cityB.GetComponent<CityStorage>().sprites[2];
 
-            cityA.GetComponent<CityStorage>().timeToAddRock += 1;
+            // cityA.GetComponent<CityStorage>().timeToAddRock += 1;
         }
         //daca cityB are piatra si cityA nu
         else if(!cityA.GetComponent<CityStorage>().resourcesList.Contains("Rock") && cityB.GetComponent<CityStorage>().resourcesList.Contains("Rock"))
         {
             cityA.GetComponent<CityStorage>().resourcesList.Add("Rock");
-            cityA.GetComponent<CityStorage>().timeToAddRock += 1;
-            cityA.GetComponent<CityStorage>().canAddRock=true;
+            // cityA.GetComponent<CityStorage>().timeToAddRock += 1;
+            // cityA.GetComponent<CityStorage>().canAddRock=true;
             GameObject resource = Instantiate(resourceIcon, cityA.GetComponent<CityStorage>().iconsParent);
             resource.GetComponentInChildren<Image>().sprite = cityA.GetComponent<CityStorage>().sprites[2];
             
-            cityB.GetComponent<CityStorage>().timeToAddRock += 1;
+            // cityB.GetComponent<CityStorage>().timeToAddRock += 1;
         }
 
         //daca cityA are nisip si cityB nu
         if(cityA.GetComponent<CityStorage>().resourcesList.Contains("Sand") && !cityB.GetComponent<CityStorage>().resourcesList.Contains("Sand"))
         {
             cityB.GetComponent<CityStorage>().resourcesList.Add("Sand");
-            cityB.GetComponent<CityStorage>().timeToAddSand += 1;
-            cityB.GetComponent<CityStorage>().canAddSand=true;
+            // cityB.GetComponent<CityStorage>().timeToAddSand += 1;
+            // cityB.GetComponent<CityStorage>().canAddSand=true;
             GameObject resource = Instantiate(resourceIcon, cityB.GetComponent<CityStorage>().iconsParent);
             resource.GetComponentInChildren<Image>().sprite = cityB.GetComponent<CityStorage>().sprites[3];
 
-            cityA.GetComponent<CityStorage>().timeToAddSand += 1;
+            // cityA.GetComponent<CityStorage>().timeToAddSand += 1;
         }
         //daca cityB are nisip si cityA nu
         else if(!cityA.GetComponent<CityStorage>().resourcesList.Contains("Sand") && cityB.GetComponent<CityStorage>().resourcesList.Contains("Sand"))
         {
             cityA.GetComponent<CityStorage>().resourcesList.Add("Sand");
-            cityA.GetComponent<CityStorage>().timeToAddSand += 1;
-            cityA.GetComponent<CityStorage>().canAddSand=true;
+            // cityA.GetComponent<CityStorage>().timeToAddSand += 1;
+            // cityA.GetComponent<CityStorage>().canAddSand=true;
             GameObject resource = Instantiate(resourceIcon, cityA.GetComponent<CityStorage>().iconsParent);
             resource.GetComponentInChildren<Image>().sprite = cityA.GetComponent<CityStorage>().sprites[3];
 
-            cityB.GetComponent<CityStorage>().timeToAddSand += 1;
+            // cityB.GetComponent<CityStorage>().timeToAddSand += 1;
         }
     }
 }
